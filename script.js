2e39aede298afc981dcffe9fa92a8f77ea488f1f
Angela Mitchell <anglarenmit@gmail.com>
2022-10-01 22:43:26 -0500

    highlight current day

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const DAYS = {
+  0: "sun",
+  1: "mon",
+  2: "tue",
+  3: "wed",
+  4: "thu",
+  5: "fri",
+  6: "sat",
+};
+
 window.addEventListener("load", () => {
   fetch("./data.json").then((response) => {
     response.json().then((json) => {
@@ -9,6 +19,7 @@ window.addEventListener("load", () => {
 function appendChartData(json) {
   const tbody = document.getElementById("spending-chart__body");
   const highestExpense = calculateHighestExpense(json);
+  const currentDay = new Date().getDay();
 
   json.forEach((expense, index) => {
     const roundedExpense = Math.round(expense.amount);
@@ -16,18 +27,14 @@ function appendChartData(json) {
     tbody.innerHTML += `
         <tr data-id="expense-${index + 1}">
             <th scope="row">${expense.day}</th>
-            <td id="expense-amount" style="--size: calc(${roundedExpense}/ ${Math.round(
+            <td style="--size: calc(${roundedExpense}/ ${Math.round(
       highestExpense
     )})"><span class="data">${expense.amount}</span></td>
         </tr>
     `;
 
-    if (expense.amount === highestExpense) {
-      const highestExpenseElement = document.querySelector(
-        `[data-id="expense-${index + 1}"]`
-      );
-      const highestExpenseBar = highestExpenseElement.querySelector("#expense-amount");
-      highestExpenseBar.classList.add("spending-card__highest-expense");
+    if (expense.day === DAYS[currentDay]) {
+      highlightCurrentDay(`expense-${index + 1}`);
     }
   });
 }
@@ -35,4 +42,9 @@ function appendChartData(json) {
 function calculateHighestExpense(json) {
   // map creates an array of all of the expense amonuts
   return Math.max(...json.map((expense) => expense.amount));
+}
+
+function highlightCurrentDay(dataId) {
+  const bar = document.querySelector(`tr[data-id="${dataId}"] td`);
+  bar.classList.add("spending-card__current-day");
 }
